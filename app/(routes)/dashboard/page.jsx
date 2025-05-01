626f40c7ae18d5e6313f96ccc11567b664c1e079
Nandini-13 <nandinigangadharan13@gmail.com>
2025-05-01 17:52:38 +0530

    Dashboard cards

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,100 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+import { UserButton} from '@clerk/nextjs'
 function dashboard() {
-  return (
-    <div>
-      dashboard
+  return (      
+    <div className="bg-teal-50 py-20 sm:py-12">
+      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-center text-4xl font-semibold text-teal-950 sm:text-5xl">Hi, User</h2>
+        <p className="mx-auto mt-2 max-w-lg text-center text-3xl tracking-tight text-balance text-gray-700 sm:text-4xl">
+          Hisaab Kitaab — budget, spend, and smart tips, all in one spot!
+        </p>
+        <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Number of Budgets
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                You’ve created multiple budgets to manage different categories. This helps you organize your spending — like groceries, rent, travel, and more.                </p>
+              </div>
+              <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset top-10 bottom-0 overflow-hidden">
+                  <img
+                    className="size-full object-cover object-top"
+                    src="https://img.freepik.com/free-vector/personal-finance-concept-illustration_114360-5224.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Total Budget</p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                This is the total amount you've planned to spend. Keep track of your overall financial capacity and stay on top of your budgeting goals.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+                <img
+                  className="w-full max-lg:max-w-xs"
+                  src="https://img.freepik.com/free-vector/money-income-concept-illustration_114360-6989.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute inset-px rounded-lg bg-white"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Total Spend</p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                Here's how much you've spent so far. Monitor your expenses to make sure you're staying within your planned limits.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+                <img
+                  className="h-[min(152px,40cqw)] object-cover"
+                  src="https://img.freepik.com/free-vector/money-loss-concept-illustration_114360-20113.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5"></div>
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Financial Guide
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                Need help managing your money? Check out expert tips and resources tailored to help you budget better and spend smarter.                </p>
+              </div>
+              <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset top-10 bottom-0 overflow-hidden">
+                  <img
+                    className="size-full object-cover object-top"
+                    src="https://img.freepik.com/free-vector/financial-advisor-concept-illustration_114360-16973.jpg?semt=ais_hybrid&w=740"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+          </div>
+        </div>
+      </div>
     </div>
+  
   )
 }
 
--- app/pages/HomePage.jsx
+++ app/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import HeroSection from "./_components/HeroSection";
-import Navbar from "./_components/Navbar";
-import About from './_components/About';
+import HeroSection from "../_components/HeroSection";
+import Navbar from "../_components/Navbar";
+import About from '../_components/About';
 export default function HomePage(){
   return (
     <div>
